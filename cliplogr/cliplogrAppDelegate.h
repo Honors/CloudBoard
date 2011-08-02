7016ca77948e1969b830a2e74fc78940c25adc3b
Matt Neary <neary.matt@gmail.com>
2011-08-02 19:56:12 -0400

    Initial Commit

---
+++ cliplogr/cliplogrAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  cliplogrAppDelegate.h
+//  cliplogr
+//
+//  Created by Matt on 8/2/11.
+//  Copyright 2011 St. Timothy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cliplogrAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
